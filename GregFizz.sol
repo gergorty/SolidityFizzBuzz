f3e59212dc0bac7af3471c349e46fa374084dd32
Greg <greg.galetto@gmail.com>
2023-01-31 15:07:43 -0500

    first commit

---
+++ GregFizz.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract FizzBuzz {
+
+    function check(uint _x) public view returns (string memory) {
+        require(_x >= 0, "Must be positive integer");
+
+        if (_x % 3 == 0 && _x % 5 == 0){
+            return "FizzBuzz";
+        }else if (_x % 3 == 0){
+            return "Fizz";
+        }else if (_x % 5 == 0){
+            return "Buzz";
+        }else{
+            return "Not a Fizz Buzz";
+        }
+
+    }
+}
